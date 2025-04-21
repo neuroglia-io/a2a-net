943baf75c57cc893b9255db41bed4c4f05ad0945
Charles d'Avernas <charles.davernas@neuroglia.io>
2025-04-21 19:54:05 +0200

    feat: add a new `SemanticKernel`-based sample, including both server and client apps
    Signed-off-by: Charles d'Avernas <charles.davernas@neuroglia.io>

---
+++ samples/semantic-kernel/a2a-net.Samples.SemanticKernel.Server/Extensions/IAsyncEnumerableExtensions.cs
@@ -0,0 +1,45 @@
+﻿// Copyright � 2025-Present the a2a-net Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace A2A.Samples.SemanticKernel.Server;
+
+/// <summary>
+/// Defines extensions for <see cref="IAsyncEnumerable{T}"/> instances
+/// </summary>
+public static class IAsyncEnumerableExtensions
+{
+
+    /// <summary>
+    /// Enumerates the elements of an <see cref="IAsyncEnumerable{T}"/> while providing a lookahead to the next item.
+    /// Useful for scenarios where you need to detect the final iteration or inspect the upcoming element.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source asynchronous enumerable to iterate.</param>
+    /// <param name="cancellationToken">A token to cancel the enumeration operation.</param>
+    /// <returns>
+    /// An <see cref="IAsyncEnumerable{T}"/> of tuples, where each tuple contains the current item and the next item (or <c>default</c> if at the end of the sequence).
+    /// </returns>
+    public static async IAsyncEnumerable<(T current, T? next)> PeekingAsync<T>(this IAsyncEnumerable<T> source, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        await using var enumerator = source.GetAsyncEnumerator(cancellationToken);
+        if (!await enumerator.MoveNextAsync()) yield break;
+        var current = enumerator.Current;
+        while (await enumerator.MoveNextAsync())
+        {
+            yield return (current, enumerator.Current);
+            current = enumerator.Current;
+        }
+        yield return (current, default);
+    }
+
+}
---
+++ samples/semantic-kernel/a2a-net.Samples.SemanticKernel.Server/Program.cs
@@ -0,0 +1,61 @@
+// Copyright � 2025-Present the a2a-net Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"),
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var builder = WebApplication.CreateBuilder(args);
+var applicationOptions = new ApplicationOptions();
+builder.Configuration.Bind(applicationOptions);
+builder.Services
+    .AddOptions<ApplicationOptions>()
+    .Bind(builder.Configuration)
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+});
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddTransient<IAgentRuntime, AgentRuntime>();
+builder.Services.AddA2AWellKnownAgent((provider, builder) =>
+{
+    builder
+        .WithName(applicationOptions.Agent.Name)
+        .WithDescription(applicationOptions.Agent.Description!)
+        .WithVersion(applicationOptions.Agent.Version)
+        .WithProvider(provider => provider
+            .WithOrganization("a2a-net")
+            .WithUrl(new("https://github.com/neuroglia-io/a2a-net")))
+        .WithUrl(new("/a2a", UriKind.Relative));
+    if (applicationOptions.Agent.Skills != null)
+    {
+        foreach (var skill in applicationOptions.Agent.Skills)
+        {
+            builder
+               .WithSkill(skillBuilder => skillBuilder
+                   .WithId(skill.Id)
+                   .WithName(skill.Name)
+                   .WithDescription(skill.Description!));
+        }
+    }
+});
+builder.Services.AddA2AProtocolServer(builder =>
+{
+    builder
+        .UseAgentRuntime(provider => provider.GetRequiredService<IAgentRuntime>())
+        .UseDistributedCacheTaskRepository();
+});
+var app = builder.Build();
+
+app.MapA2AWellKnownAgentEndpoint();
+app.MapA2AAgentHttpEndpoint("/a2a");
+
+await app.RunAsync();
